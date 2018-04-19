c04a4955fde149361e9cafce4e0ea5b202ba5a50
wu_ze_qing <wuzeqing@zuzuche.com>
2018-04-19 11:00:51 +0800

    :rocket: 添加可操作添加模块功能

--- src/main.js
+++ src/main.js
@@ -5,18 +5,41 @@ class HotSpot {
         console.log(parent);
         this.parent          = parent; //绑定的父元素
         this.parentPos       = {}; //父元素位置
-        this.option          = option; 
+        this.container       = null; //控制面板输出容器
+
+        this.option          = Object.assign({
+                                   container: 'list' //容器id
+                               }, option); 
         this.startPos        = {};//开始绘制的位置
         this.nowPos          = {}; //目前的鼠标位置
         this.activeChildNode = null; //激活的子元素
+        this.childNodeList   = []; //选择的元素
         this.STATUS          = '';
 
         this._init();
-        this._getParentPosition(this.parent);
     }
 
     _init() {
+        this._initVariable();
         this._initEvent();
+    }
+
+    /**
+     * 初始化变量
+     * 
+     * @memberof HotSpot
+     */
+    _initVariable() {
+        this._getParentPosition(this.parent);
+        this.container = document.querySelector(this.option.container);
+
+        //判断页面是否存在
+        if(!this.container) {
+            let createElement = document.createElement('ul');
+            createElement.id = this.option.container;
+            document.body.appendChild(createElement);
+            this.container = createElement;
+        }
     }
 
     /**
@@ -28,6 +51,8 @@ class HotSpot {
         this.parent.addEventListener('mousedown', this._mouseDown.bind(this));
         this.parent.addEventListener('mousemove', this._mouseMove.bind(this));
         this.parent.addEventListener('mouseup', this._mouseUp.bind(this));
+
+        this.container.addEventListener('click', this._childClickEvent.bind(this));
     }
 
     /**
@@ -54,7 +79,7 @@ class HotSpot {
         }
         this._getMousePosition(e);
         let result = this._calculateWidthAndHeight();
-        console.log(result);
+        
         this.activeChildNode.style.cssText = `
             position: absolute;
             left: ${this.nowPos.x - this.startPos.x < 0 ? this.nowPos.x : this.startPos.x}px;
@@ -70,8 +95,49 @@ class HotSpot {
      * 
      * @memberof HotSpot
      */
-    _mouseUp() {
+    _mouseUp(e) {
         this.STATUS = 'end';
+    }
+
+    /**
+     * 子元素生成控制列表点击事件处理
+     * 
+     * @param {any} e 
+     * @memberof HotSpot
+     */
+    _childClickEvent(e) {
+        e.stopPropagation();
+        console.log(e);
+        let target = e.target;
+        let hash   = e.target.dataset.hash;
+
+        //判断点击元素类型
+        if(target.dataset.type == 'del') {
+            this._removeRectangle(hash);
+            this._removeLiElement(target);
+        }else if(target.dataset.type == 'sure') {
+            this._bindRectangleEvent(target);
+        }
+    }
+
+    /**
+     * 绑定点击事件
+     * 
+     * @param {any} filter 
+     * @memberof HotSpot
+     */
+    _bindRectangleEvent(target) {
+        let parent     = target.parentNode;
+        let urlELement = parent.querySelector('[data-url]');
+        console.log(urlELement);
+
+        let urlLink = urlELement.value;
+        if(!urlLink) {
+            alert('还没填写url');
+            return false;
+        }
+
+        
     }
 
     /**
@@ -96,6 +162,20 @@ class HotSpot {
      */
     _createRectangle() {
         let element = document.createElement('div');
+        let liElement = document.createElement('li');
+        let timeHash = new Date().getTime().toString();
+        liElement.innerHTML = `
+            <div class="child__node">
+                <span class="child__node__text">
+                    这是第${this.childNodeList.length + 1}个区域
+                </span>
+                <input type="text" data-url placeholder="输入绑定的url">
+                <button data-hash='${timeHash}' data-type='sure'>确认</button>
+                <button data-hash='${timeHash}' data-type='edit'>编辑</button>
+                <button data-hash='${timeHash}' data-type='del'>删除</button>
+            </div>
+        `
+
         element.style.cssText = `
             position: absolute;
             left: ${this.startPos.x}px;
@@ -104,8 +184,75 @@ class HotSpot {
             height: 0;
             border: 1px solid #ccc;
         `
+
+        let childNodeObj = {
+            hash: timeHash,
+            element
+        }
+
         this.parent.appendChild(element);
+        this.container.appendChild(liElement);
+        this.childNodeList.push(childNodeObj);
         this.activeChildNode = element;
+    }
+
+    /**
+     * 删除已生成的矩形区域
+     * 
+     * @param {any} filter 
+     * @memberof HotSpot
+     */
+    _removeRectangle(hash) {
+        let filter = this.childNodeList.find((item) => {
+            return item.hash === hash;
+        })
+
+        this.parent.removeChild(filter['element'])
+    }
+
+    /**
+     * 删除控制台生成的对应选项
+     * 
+     * @param {any} target 
+     * @memberof HotSpot
+     */
+    _removeLiElement(target) {
+        let parent = this._findParent('li', target);
+        this.container.removeChild(parent);
+    }
+
+    /**
+     * 寻找父元素
+     * 
+     * @param {any} tagName 
+     * @param {any} el 
+     * @returns 
+     * @memberof HotSpot
+     */
+    _findParent(tagName, el) {
+        try {
+            el = target.closest('li');
+        }catch (err) {
+            while (el && el.tagName !== tagName.toUpperCase()) {
+                el = el.parentNode;
+            }
+        }
+        
+        return el;
+    }
+
+    /**
+     * 获取临近元素
+     * 
+     * @param {any} filter 
+     * @param {any} el 
+     * @returns 
+     * @memberof HotSpot
+     */
+    _findNearElement(filter, el) {
+        let result = el.querySelector(filter);
+
+        return result;
     }
 
     /**
